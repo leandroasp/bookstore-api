549cf9071a8ceab3ce90a30218b82f6885c96f50
Leandro Sales <leandroasp@gmail.com>
2021-08-22 21:43:32 -0300

    Update para Categoria

--- src/main/java/me/urbanize/bookstore/service/CategoriaService.java
+++ src/main/java/me/urbanize/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import me.urbanize.bookstore.domain.Categoria;
+import me.urbanize.bookstore.dtos.CategoriaDTO;
 import me.urbanize.bookstore.repositories.CategoriaRepository;
 import me.urbanize.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -28,6 +29,13 @@ public class CategoriaService {
 
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
 	}
 }
